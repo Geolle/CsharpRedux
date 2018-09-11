bc7539dfa8559d5188bd9d86236d205520a48ddf
Smile_Gum_Gum <fcliqinggang@gmail.com>
2018-09-11 12:38:38 +0800

    Init

---
+++ CsharpRedux/Web/Redux/State.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Redux
+{
+    public abstract class State
+    {
+
+        public abstract string Key { get; }
+
+        public abstract object Value { get; }
+
+    }
+}
